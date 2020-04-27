555e968b79e000db85dd46541d6e61cc2d12860b
kueselj <kuesel.j@husky.neu.edu>
2020-04-27 00:32:40 -0400

    Start implementation of model.
    Added the constructor for the ArrayModel.

--- src/model/ArrayModel.java
+++ src/model/ArrayModel.java
@@ -1,4 +1,31 @@
 package model;
 
+import java.util.*;
+
+/**
+ * Represents the model which is an array.
+ */
 public class ArrayModel {
+
+  private final Integer[] integers;
+
+  /**
+   * Basic constructor for an ArrayModel. Returns a randomly ordered array with integers from 1
+   * to the specified integer.
+   * @param numIntegers the size of the array.
+   */
+  public ArrayModel(int numIntegers) {
+    List<Integer> temp = new ArrayList<Integer>();
+
+    for (int i = 0; i < numIntegers; i ++) {
+      temp.add(i + 1);
+    }
+    Collections.shuffle(temp);
+
+    this.integers = temp.toArray(new Integer[temp.size()]);
+    System.out.println(this.integers);
+
+  }
+
+
 }
